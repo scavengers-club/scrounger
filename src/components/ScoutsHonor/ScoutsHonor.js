66d48901ccb12b3735938dd7a91e5097f98f781a
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-18 15:50:45 -0800

    scouts honor routes properly

--- src/components/ScoutsHonor/ScoutsHonor.js
+++ src/components/ScoutsHonor/ScoutsHonor.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 //this page will appear only when a new user is created
 //will prompt user to agree to basic terms and conitions, verify age, etc
 
 export default function ScoutsHonor() {
+  const [checked, setChecked] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = async () => {
+    if (checked) {
+      history.push('/');
+    } else {
+      alert('you must agree to scouts honor');
+    }
+  };
+
   return (
     <div>
-      <p>here are all the guidlines we want you to follow</p>
+      <p>
+        Please be respectful of others and property. Leave things as you find them. Be responsible
+        if using this app after dark. Carry a mask so as to keep yourself and others safe. If you
+        are adventuring alone, make sure someone knows where you are. Happy Adventuring!
+      </p>
+      <label>
+        <input type="checkbox" isChecked={checked} onChange={() => setChecked(!checked)} />
+        <span>I have read and agree to the Scouts Honor Pledge</span>
+      </label>
+      <button onClick={handleSubmit}>Lets Explore</button>
     </div>
   );
 }
