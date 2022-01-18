30c4d3e845d46cf22f0e7f0f58889ad7c9afaef2
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-18 15:42:08 -0800

    rendering adventures as a list on home page

--- src/components/AdventureList/AdventureList.js
+++ src/components/AdventureList/AdventureList.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 //this will render all of the adventures onto to home page
 
-export default function AdventureList() {
-  return <div></div>;
+export default function AdventureList({ adventures }) {
+  return (
+    <div className="adventure-list">
+      {adventures.map((adventure) => (
+        <div key={adventure.id}>
+          <h3>{adventure.name}</h3>
+          <img src={adventure.image}></img>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import AdventureList from '../../components/AdventureList/AdventureList';
 import { getAdventures } from '../../services/adventures';
 
-export default function Home() {
+export default function Home({ currentUser }) {
   const [adventures, setAdventures] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -13,22 +14,24 @@ export default function Home() {
     };
     fetchData();
   }, []);
-  console.log(adventures);
+
   return (
     <div>
-      <h2>Scrounger</h2>
-      <p>
-        This is a description of the app for those who do not have an account or are not logged in
-        to see
-      </p>
-      <div className="adventure-list">
-        {adventures.map((adventure) => (
-          <div key={adventure.id}>
-            <h3>{adventure.name}</h3>
-            <img src={adventure.image}></img>
-          </div>
-        ))}
-      </div>
+      {!currentUser && (
+        <>
+          <h2>Scrounger</h2>
+          <p>
+            This is a description of the app for those who do not have an account or are not logged
+            in to see
+          </p>
+        </>
+      )}
+      {currentUser && (
+        <>
+          <h2>Welcome back!</h2>
+        </>
+      )}
+      <AdventureList {...{ adventures }} />
     </div>
   );
 }
