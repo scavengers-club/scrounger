2ede01b19cfd7186cee720402c99483ee35619e1
Violet Stewart <vkazoo2@gmail.com>
2022-01-18 11:50:07 -0800

    started log in form

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -3,6 +3,18 @@ import React from 'react';
 //this will be the form a returning user sees
 //will ask for username and password
 
-export default function LogIn() {
-  return <div></div>;
+export default function LogInForm() {
+  return (
+    <form>
+      <label>
+        username:
+        <input type="text" />
+      </label>
+      <label>
+        password:
+        <input type="password" />
+      </label>
+      <button>Log In</button>
+    </form>
+  );
 }
