2fa1672176e5c3a98356fef578b91949184ea492
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-18 12:14:32 -0800

    log in/registration form set up

--- src/views/Auth/Register.js
+++ src/views/Auth/Register.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
 
 export default function Register() {
   return (
     <div>
       <h1>Register</h1>
+      <RegisterForm />
     </div>
   );
 }
