bba8643050b6385239513eb3663cbe13675ec4c6
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-18 12:53:49 -0800

    register page working, communicating with Supabase

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -6,30 +6,60 @@ import { Link } from 'react-router-dom';
 //*strong possibility of avatar option*
 //on button click here, will call scout honor and require second button click to complete registration
 
-export default function RegisterForm() {
+export default function RegisterForm({
+  username,
+  setUsername,
+  email,
+  setEmail,
+  password,
+  setPassword,
+  checked,
+  setChecked,
+  handleSubmit,
+}) {
   return (
     <>
       <form>
         <label>
           username:
-          <input type="text" />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="make it unique"
+          />
         </label>
         <label>
           email:
-          <input type="email;" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email@example.com"
+          />
         </label>
         <label>
           password:
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="make it strong"
+          />
         </label>
+
+        {/* check if input value === password */}
         {/* <label>
           re-enter password:
           <input type="password" />
         </label> */}
+
+        {/* create logic to require checkbox isChecked */}
         <label>
-          <input type="checkbox" />I am over the age of 13
+          <input type="checkbox" isChecked={checked} onChange={() => setChecked(!checked)} />
+          <span>I am over the age of 13</span>
         </label>
-        <button>Register</button>
+        <button onClick={handleSubmit}>Register</button>
       </form>
       <p>Already have an account?</p>
       <Link to="/login">Log In</Link>
--- src/views/Auth/Register.js
+++ src/views/Auth/Register.js
@@ -1,16 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
+import { registerUser } from '../../services/users';
 
 export default function Register({ setCurrentUser }) {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const [checked, setChecked] = useState(false);
 
-  useEffect(() => {});
+  const handleSubmit = async (e) => {
+    if (checked) {
+      e.preventDefault();
+      const resp = await registerUser(email, password);
+      setCurrentUser(resp);
+    } else {
+      alert('you must check that box');
+    }
+  };
+
   return (
     <div>
       <h1>Register</h1>
-      <RegisterForm />
+      <RegisterForm
+        {...{
+          username,
+          setUsername,
+          email,
+          setEmail,
+          password,
+          setPassword,
+          checked,
+          setChecked,
+          handleSubmit,
+        }}
+      />
     </div>
   );
 }
