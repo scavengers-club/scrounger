128e0adaec82a944e32871e699f520195c0a56d8
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-20 21:56:32 -0800

    we wrote that log in form test yo

---
+++ src/components/LogInForm/LogInForm.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import LogInForm from './LogInForm';
+
+test('<LogInForm/> should display an empty form', () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/login']}>
+      <Route path="/login">
+        <LogInForm />
+      </Route>
+    </MemoryRouter>
+  );
+
+  screen.getAllByDisplayValue('');
+  expect(container).toMatchSnapshot();
+});
